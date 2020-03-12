0a4f58e4213f0c84702567c33a4ab14cec308098
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 13:44:12 +0600

    :zap: Add bloc skeleton files

---
+++ counter/lib/bloc/counter/counter_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:counter/bloc/counter/counter_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvent, int> {
+  //Bloc<input: event, output:state>
+
+
+
+}
