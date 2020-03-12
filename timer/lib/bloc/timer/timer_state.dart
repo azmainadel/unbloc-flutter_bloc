ded48def054094b17214b48e0ae5bc58f769c186
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 15:21:27 +0600

    :zap: Add timer states

--- timer/lib/bloc/timer/timer_state.dart
+++ timer/lib/bloc/timer/timer_state.dart
@@ -1,10 +1,39 @@
 import 'package:equatable/equatable.dart';
 
 abstract class TimerState extends Equatable {
-  const TimerState();
+  final int duration;
+
+  const TimerState(this.duration);
+
+  @override
+  List<Object> get props => [duration];
 }
 
-class InitialTimerState extends TimerState {
+//Ready to start counting
+class Ready extends TimerState {
+  const Ready(int duration) : super(duration);
+
   @override
-  List<Object> get props => [];
+  String toString() => 'Ready { duration: $duration }';
+}
+
+//Paused on a count
+class Paused extends TimerState {
+  const Paused(int duration) : super(duration);
+
+  @override
+  String toString() => 'Paused { duration: $duration }';
+}
+
+//Counting going on
+class Running extends TimerState {
+  const Running(int duration) : super(duration);
+
+  @override
+  String toString() => 'Running { duration: $duration }';
+}
+
+//Counting finished for the given value
+class Finished extends TimerState {
+  const Finished() : super(0);
 }
