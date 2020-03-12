c4780dbc9c7a94f316bf0649d0ec07f03911552c
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 15:37:30 +0600

    :zap: Add timer bloc skeleton

--- timer/lib/bloc/timer/timer_bloc.dart
+++ timer/lib/bloc/timer/timer_bloc.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-;
 
 import '../bloc.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  @override
-  TimerState get initialState => InitialTimerState();
+  final int _remainingTicks = 60;
 
   @override
-  Stream<TimerState> mapEventToState(
-    TimerEvent event,
-  ) async* {
-    // TODO: Add Logic
+  TimerState get initialState => Ready(_remainingTicks);
+
+  @override
+  Stream<TimerState> mapEventToState(TimerEvent event) async* {
+    if (event is Start) {
+    } else if (event is Tick) {
+    } else if (event is Pause) {
+    } else if (event is Resume) {
+    } else if (event is Reset) {}
   }
 }
