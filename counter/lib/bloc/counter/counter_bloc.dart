39b563b0004cc04f46b51161bf7c90600c40937e
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 13:45:25 +0600

    :zap: Add initial state

--- counter/lib/bloc/counter/counter_bloc.dart
+++ counter/lib/bloc/counter/counter_bloc.dart
@@ -4,6 +4,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CounterBloc extends Bloc<CounterEvent, int> {
   //Bloc<input: event, output:state>
 
+  //Always set the initial state. Here the state is an integer.
+  //So we start the count from 0
+  @override
+  int get initialState => 0;
 
 
 }
