ffbe9090749d487d317154a4a6446d0f87387175
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 17:06:14 +0600

    :art: Add action button row component

---
+++ timer/lib/ui/components/action_buttons.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer/bloc/bloc.dart';
+
+class ActionButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children:
+          _mapStateToButtons(timerBloc: BlocProvider.of<TimerBloc>(context)),
+    );
+  }
+
+  List<Widget> _mapStateToButtons({TimerBloc timerBloc}) {
+    final TimerState currentState = timerBloc.state;
+    final int remainingTicks = currentState.remainingTicks;
+
+    if (currentState is Ready) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.add(Start(remainingTicks: remainingTicks)),
+        ),
+      ];
+    } else if (currentState is Running) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.pause),
+          onPressed: () => timerBloc.add(Pause(remainingTicks: remainingTicks)),
+        ),
+      ];
+    } else if (currentState is Paused) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc
+              .add(Resume(remainingTicks: currentState.remainingTicks)),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () =>
+              timerBloc.add(Reset(remainingTicks: currentState.remainingTicks)),
+        ),
+      ];
+    } else if (currentState is Finished) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () =>
+              timerBloc.add(Reset(remainingTicks: currentState.remainingTicks)),
+        ),
+      ];
+    }
+
+    return [];
+  }
+}
