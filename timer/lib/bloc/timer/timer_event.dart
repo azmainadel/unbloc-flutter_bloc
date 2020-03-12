d3881e8ea373adead7160a3a4d635d38a0517ff7
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 15:27:45 +0600

    :zap: Add timer events

--- timer/lib/bloc/timer/timer_event.dart
+++ timer/lib/bloc/timer/timer_event.dart
@@ -1,5 +1,36 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 abstract class TimerEvent extends Equatable {
   const TimerEvent();
+
+  @override
+  List<Object> get props => [];
 }
+
+class Start extends TimerEvent {
+  final int remainingTicks;
+
+  const Start({@required this.remainingTicks});
+
+  @override
+  String toString() => "Started for $remainingTicks ticks";
+}
+
+class Tick extends TimerEvent {
+  final int remainingTicks;
+
+  const Tick({@required this.remainingTicks});
+
+  @override
+  List<Object> get props => [remainingTicks];
+
+  @override
+  String toString() => "Tick $remainingTicks";
+}
+
+class Pause extends TimerEvent {}
+
+class Resume extends TimerEvent {}
+
+class Reset extends TimerEvent {}
