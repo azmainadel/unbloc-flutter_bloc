fb09a2056f7f336fa75eeaab856ee0df4fe6967c
Azmain Adel <azmainadel47@gmail.com>
2020-03-12 15:22:52 +0600

    :art: Rename fields

--- timer/lib/bloc/timer/timer_state.dart
+++ timer/lib/bloc/timer/timer_state.dart
@@ -1,39 +1,39 @@
 import 'package:equatable/equatable.dart';
 
 abstract class TimerState extends Equatable {
-  final int duration;
+  final int remainingTime;
 
-  const TimerState(this.duration);
+  const TimerState({this.remainingTime});
 
   @override
-  List<Object> get props => [duration];
+  List<Object> get props => [remainingTime];
 }
 
 //Ready to start counting
 class Ready extends TimerState {
-  const Ready(int duration) : super(duration);
+  const Ready(int duration) : super(remainingTime: duration);
 
   @override
-  String toString() => 'Ready { duration: $duration }';
+  String toString() => 'Ready { duration: $remainingTime }';
 }
 
 //Paused on a count
 class Paused extends TimerState {
-  const Paused(int duration) : super(duration);
+  const Paused(int duration) : super(remainingTime: duration);
 
   @override
-  String toString() => 'Paused { duration: $duration }';
+  String toString() => 'Paused { duration: $remainingTime }';
 }
 
 //Counting going on
 class Running extends TimerState {
-  const Running(int duration) : super(duration);
+  const Running(int duration) : super(remainingTime: duration);
 
   @override
-  String toString() => 'Running { duration: $duration }';
+  String toString() => 'Running { duration: $remainingTime }';
 }
 
 //Counting finished for the given value
 class Finished extends TimerState {
-  const Finished() : super(0);
+  const Finished() : super(remainingTime: 0);
 }
