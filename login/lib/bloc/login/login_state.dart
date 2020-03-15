dbf64905419b690eeb585a21319a2baf3836844f
Azmain Adel <azmainadel47@gmail.com>
2020-03-15 15:46:27 +0600

    :zap: Add login states

--- login/lib/bloc/login/login_state.dart
+++ login/lib/bloc/login/login_state.dart
@@ -1,10 +1,22 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
 
 abstract class LoginState extends Equatable {
   const LoginState();
-}
 
-class InitialLoginState extends LoginState {
   @override
   List<Object> get props => [];
+}
+
+class LoginInitialState extends LoginState {}
+
+class LoginLoadingState extends LoginState {}
+
+class LoginFailureState extends LoginState {
+  final String errorText;
+
+  const LoginFailureState({@required this.errorText});
+
+  @override
+  List<Object> get props => [errorText];
 }
