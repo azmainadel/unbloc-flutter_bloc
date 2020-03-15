65333d922073bfd2ae49738b8a056bc786c1e3b2
Azmain Adel <azmainadel47@gmail.com>
2020-03-15 15:18:08 +0600

    :zap: Map event and states in bloc

--- login/lib/bloc/authentication/authentication_bloc.dart
+++ login/lib/bloc/authentication/authentication_bloc.dart
@@ -1,22 +1,46 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:login/data/user_repository.dart';
 
 import '../bloc.dart';
 
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null);
+
   @override
   AuthenticationState get initialState => Uninitialized();
 
   @override
   Stream<AuthenticationState> mapEventToState(
-    AuthenticationEvent event,
-  ) async* {
-    if (event is AppStarted) {}
+      AuthenticationEvent event) async* {
+    if (event is AppStarted) {
+      final bool hasToken = await userRepository.hasToken();
 
-    if (event is LoggedIn) {}
+      if (hasToken) {
+        yield Authenticated();
+      } else {
+        yield Unauthenticated();
+      }
+    }
 
-    if (event is LoggedOut) {}
+    if (event is LoggedIn) {
+      yield Initializing();
+
+      await userRepository.persistToken(event.token);
+      yield Authenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield Initializing();
+
+      await userRepository.deleteToken();
+      yield Unauthenticated();
+    }
   }
 }
