098c9974a00c3b7397bf66ff267cb2ffd450409e
Azmain Adel <azmainadel47@gmail.com>
2020-03-15 15:50:50 +0600

    :zap: Create login bloc skeleton

--- login/lib/bloc/login/login_bloc.dart
+++ login/lib/bloc/login/login_bloc.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:login/data/user_repository.dart';
 
 import '../bloc.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({@required this.userRepository, @required this.authenticationBloc})
+      : assert(userRepository != null),
+        assert(userRepository != null);
+
   @override
   LoginState get initialState => LoginInitialState();
 
@@ -12,6 +21,6 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    // TODO: Add Logic
+    if (event is LoginPressed) {}
   }
 }
